cad9e5fd8fa12269aab08a0b602850a12d7bacd9
Ivan <ivandiaz.cont@gmail.com>
2026-08-28 13:25:34 -0600

    Prueba

--- frontend/src/components/admin/AdminTopbar.jsx
+++ frontend/src/components/admin/AdminTopbar.jsx
@@ -18,7 +18,7 @@ function AdminTopbar({
     <header className="topbar">
       <div>
         <p className="section-label">
-          PANEL DEL ADMINISTRADOR
+          PANEL DEL ADMINISTRADORA
         </p>
 
         <h2>{titulo}</h2>
